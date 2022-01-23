53779487b26d2f61309339e0eeb8a2edc5e4ee3f
or abdillh <hans.abdillh05@gmail.com>
2022-01-23 19:58:14 +0800

    Install vite-pwa

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import VueRouter from '@/router'
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+	onNeedRefresh() {},
+	onOfflineReady() {}
+})
 
 //Import Tailwind
 import './index.css'
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,48 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+  	vue(),
+  	VitePWA({
+		includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],  
+		manifest: {
+		  name: 'Scheduled App',
+		  short_name: 'Scheduled App',
+		  description: 'Scheduled App slicing design ui using Vue JS and tailwind',
+		  theme_color: '#ffffff',
+		  icons: [
+		    {
+		      src: 'pwa-192x192.png',
+		      sizes: '192x192',
+		      type: 'image/png',
+		    },
+		    {
+		      src: 'pwa-512x512.png',
+		      sizes: '512x512',
+		      type: 'image/png',
+		    },
+		    {
+		      src: 'pwa-512x512.png',
+		      sizes: '512x512',
+		      type: 'image/png',
+		      purpose: 'any maskable',
+		    }
+		  ]
+		},
+		registerType: 'autoUpdate',
+		devOptions: {
+			enabled: true
+		},
+		workbox: {
+			cleanupOutdatedCaches: false,
+			sourcemap: true
+		}
+  	})
+  ],
   define: {
      	'process.env': {}
   },
